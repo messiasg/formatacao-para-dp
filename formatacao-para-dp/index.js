4285adca5e96291c54c6865c32ffee5a80881a93
Guilherme Messias <142668009+messiasg@users.noreply.github.com>
2024-05-19 10:49:10 -0300

    Inclusão de botão para copiar texto formatado

--- formatacao-para-dp/index.js
+++ formatacao-para-dp/index.js
@@ -1,5 +1,7 @@
-const botao = document.getElementById("botao");
-botao.addEventListener("click", () => {
+// Função de formatação de texto
+
+const botaoFormatacao = document.getElementById("botaoFormatacao");
+botaoFormatacao.addEventListener("click", () => {
   try {
     // Validação do texto inserido pelo usuário
     const texto = document.getElementById("texto").value;
@@ -7,21 +9,30 @@ botao.addEventListener("click", () => {
     // Remove acentos e cedilhas
     const textoSemAcentos = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-    // Remove traços e pontos
+    // Remove traços, pontos e barras
     const textoSemTracosEPontos = textoSemAcentos.replace(/[-./]/g, "");
 
     // Converte para maiúsculas
     const textoMaiusculo = textoSemTracosEPontos.toUpperCase();
 
-    // Apresentação do resultado formatado
-    const resultado = document.getElementById("resultado");
-
     // Limpar Campos
     document.getElementById("texto").value = "";
 
+    // Apresentação do resultado formatado
+    const resultado = document.getElementById("resultado");
     resultado.innerText = textoMaiusculo;
   } catch (error) {
     alert("Erro ao processar texto:", error);
     // Exibir mensagem de erro para o usuário (opcional)
   }
+});
+
+// Função de copiar texto formatado para área de transferência
+const botaoCopiar = document.getElementById("botaoCopiar");
+botaoCopiar.addEventListener("click", () => {
+  // Seleção do conteúdo formato apresentado na página
+  const resultado = document.getElementById("resultado").textContent;
+
+  // Copiar resultado formatado
+  navigator.clipboard.writeText(resultado);
 });
